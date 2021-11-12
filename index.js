652b6f78043d3809cfb1a18354779e1984a6cc21
anlopezd <91441753+anlopezd@users.noreply.github.com>
2021-11-12 10:22:28 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+window.addEventListener("scroll", function() {
+    let header = document.getElementById("header");
+    header.classList.toggle("down", window.scrollY >= 600)
+})
+
+
+let nav = document.getElementById("header");
+
+document.getElementById("menu-container").addEventListener("click", () => {
+    nav.classList.toggle("prueba2");
+})
+
+
+
+document.getElementById("menu-container").addEventListener("click", function() {
+     let menubarra = document.getElementById("menu-container")
+    menubarra.classList.toggle("upclose")
+})
+
+window.addEventListener("scroll", function() {
+    let menu = document.getElementById("menu-container");
+    menu.classList.toggle("convertir", window.scrollY >= 509)
+})
+
+window.addEventListener("scroll", function() {
+    let menu = document.getElementById("menu-container");
+    menu.classList.toggle("fondoblanco", window.scrollY >= 4037)
+})
